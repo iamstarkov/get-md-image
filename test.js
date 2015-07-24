8a1341066e4edc3732eb16d16c354d55c170d605
Vladimir Starkov <iamstarkov@gmail.com>
2015-07-24 08:25:20 +0500

    add tests

--- test.js
+++ test.js
@@ -1,6 +1,24 @@
 import { equal } from 'assert';
-import getMdImage from './index';
+import getImage from './index';
 
-it('should getMdImage', () => {
-  equal(getMdImage('unicorns'), 'unicorns');
+const input = `
+# title
+
+description ![alt](src)
+`.trim();
+
+it('should getImage alt', () => {
+  equal(getImage(input).alt, 'alt');
+});
+
+it('should getImage src', () => {
+  equal(getImage(input).src, 'src');
+});
+
+it('should getImage empty alt', () => {
+  equal(getImage('').alt, undefined);
+});
+
+it('should getImage empty src', () => {
+  equal(getImage('').src, undefined);
 });
