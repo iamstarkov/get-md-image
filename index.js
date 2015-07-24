ec62587f2ed1df3e34ca3969af48d6d585dffea2
Vladimir Starkov <iamstarkov@gmail.com>
2015-07-24 08:26:06 +0500

    add implementation

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-export default item => item;
+import { text, match, isImage } from 'commonmark-helpers';
+
+const result = node => ({
+  alt: text(node),
+  src: node.destination,
+  node
+});
+
+export default input => {
+  const image = match(input, isImage);
+  if (!image) {
+    let alt, src, node;
+    return { alt, src, node };
+  }
+  return result(image);
+};
